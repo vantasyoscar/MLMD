8e2494ec192f731cc10f67bd6a6e3dcffa3ec937
wink1e <bin-li22@mails.tsinghua.edu.cn>
2023-05-28 16:33:21 +0800

    dnn_sym model

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
+import neighbor_list
 
 class MyDNN(nn.Module):
-    def __init__(self):
+    def __init__(self, fwd):
         super(MyDNN, self).__init__()
         self.layer1 = nn.Linear(62*3*fwd, 256)
         self.layer2 = nn.Linear(256, 128)
@@ -16,6 +17,25 @@ class MyDNN(nn.Module):
 
 
 class DNN_sym(nn.Module):
-    def __init__(self) -> None:
+    r'''
+    Creating a DNN model that is permutation invariant.
+    '''
+    def __init__(self, num_embedding: int, embedding_dim: int, linear_layers: list[int]) -> None:
         super().__init__()
-        
+        self.embedding = nn.Embedding(num_embedding, embedding_dim)
+        layers = []
+        for i in range(len(linear_layers)-1):
+            layers.append(nn.Linear(linear_layers[i], linear_layers[i+1]))
+        self.linear_layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        q = self.embedding(x)
+        d = q @ x
+        out = self.linear_layers(d)
+        return out
+    
+
+if __name__ == "__main__":
+    layers = [64, 128, 256, 3]
+    model = DNN_sym(10, 8, layers)
+    print(model)
